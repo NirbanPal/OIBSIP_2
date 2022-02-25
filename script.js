2ce840e0e01bd56bded1b7b5792fe1c8f75ba9bc
Nirban Pal <90189572+NirbanPal@users.noreply.github.com>
2022-02-26 00:30:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".showing", {
+    strings: ["Student","Designer","Web Developer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+    
---
+++ scriptabout.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".showing1", {
+    strings: ["student","Designer","Web Developer","Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
